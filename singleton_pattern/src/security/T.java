e51f875c67ab01fab3e742075033baa4e3987541
hackerAlice <sudo_rm@163.com>
2021-03-25 16:05:53 +0800

    add lazy singleton

---
+++ singleton_pattern/src/security/T.java
@@ -0,0 +1,9 @@
+package security;
+
+public class T implements Runnable {
+    @Override
+    public void run() {
+        LazySingleton lazySingleton = LazySingleton.getInstance();
+        System.out.println(Thread.currentThread().getName() + " " + lazySingleton);
+    }
+}
---
+++ singleton_pattern/src/security/Test.java
@@ -0,0 +1,11 @@
+package security;
+
+public class Test {
+    public static void main(String[] args) throws Exception{
+        Thread t1 = new Thread(new T());
+        Thread t2 = new Thread(new T());
+        t1.start();
+        t2.start();
+        System.out.println("program end");
+    }
+}
